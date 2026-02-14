d8684b6407a96a74d1ec01964543637034fc6d8b
Fullpower79 <Cedricadriaenssens11@gmail.com>
2026-02-14 17:20:39 +0100

    kleine bug fix voor deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const blogRoutes = require('./routes/blog.routes');
+const blogRoutes = require('./routes/blog-routes');
 
 const app = express();
 
